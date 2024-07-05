15805080b9164151cd967f849ad9d72b987f79b8
Lubel8A <lubelsaido@gmail.com>
2024-07-05 11:17:18 -0500

    Proyecto Enviado

---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <h1 class="title">Subir y Analizar Archivo Excel</h1>
+    <ExcelUpload />
+  </div>
+</template>
+
+<script>
+import ExcelUpload from '@/components/ExcelUpload.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    ExcelUpload,
+  },
+};
+</script>
+
+<style>
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+  text-align: center;
+}
+
+.title {
+  font-size: 2.5rem;
+  margin-bottom: 20px;
+}
+</style>
